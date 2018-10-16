47d99939695853dc7b5fdb80dd525ce41217736a
mason-smith <mason.smith94@outlook.com>
2018-10-16 10:08:17 -0700

    Flex buttons

--- src/components/home/CategoryCard.jsx
+++ src/components/home/CategoryCard.jsx
@@ -6,16 +6,22 @@ export default class CategoryCard extends React.Component {
     return (
       <div className="card animated fadeIn">
         <div className="view z-depth-2">
-          <img src={this.props.src} alt="Image" class="img-fluid" />
+          <img src={this.props.src} alt="Image" className="img-fluid" />
         </div>
         <div className="card-body card-body-cascade">
           <CardTitle className="align-middle high">
-            <img src={this.props.title} alt="Image" class="img-fluid" />
+            <img src={this.props.title} alt="Image" className="img-fluid" />
           </CardTitle>
           <CardText>{this.props.description}</CardText>
-          <a href={this.props.href} target="_blank" class="btn btn-main">
-            Visit Site
-          </a>
+          <div className="d-flex justify-content-end">
+            <a
+              href={this.props.href}
+              target="_blank"
+              className="text-center btn btn-main"
+            >
+              Visit Site
+            </a>
+          </div>
         </div>
       </div>
     );
